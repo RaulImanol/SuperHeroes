e3d37e126f89e83d5b9ff04f4c7e01bb467ae8fa
RaulImanol <80784906+RaulImanol@users.noreply.github.com>
2021-04-19 19:50:45 -0500

    Funcion borrar y mostrar implementada

--- src/app/Heroes/listado/listado.component.ts
+++ src/app/Heroes/listado/listado.component.ts
@@ -1,30 +1,24 @@
-import { Component} from '@angular/core';
-
-
+import { removeSummaryDuplicates, ThrowStmt, splitClasses } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
 
   selector: 'app-listado',
-
   templateUrl: './listado.component.html',
-
   styleUrls: ['./listado.component.css']
-
 })
+export class ListadoComponent{
 
-export class ListadoComponent {
+  heroes: string[] = ['hulk','SAITAMA','SUPERMAN','goku'];
+eliminar: string[]  = []
+indice:number = 0;
+  removeItem(i: number){
+
+    this.indice=this.indice+i;
+    this.eliminar = this.heroes.splice(i,1);
 
 
-
-  heroes: string[] = ['hulk', 'Saitama', 'Superman', 'thor', 'goku'];
-
-  borrarheroes(): string {
- return this.heroes= '';
   }
 
-    
-
-
-
-
 }
+
